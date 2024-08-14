7b6403bcd8a4e1c98030dbb50d5151c450102645
= <gabriel.cirilo.rosa@gmail.com>
2024-08-13 21:38:35 -0300

    [domain] Adicao de Annotations

--- src/main/java/com/cirion/taskManager/task/domain/Task.java
+++ src/main/java/com/cirion/taskManager/task/domain/Task.java
@@ -1,13 +1,20 @@
 package com.cirion.taskManager.task.domain;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 
 import java.util.UUID;
 
 @Entity(name = "Task")
 public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", updatable = false, unique = true, nullable = false)
     private UUID idTask;
+    @NotBlank
     private String taskName;
+    @NotEmpty
     private PriorityTask priorityTask;
     private boolean completed;
 }
