3a0f5ca6ca859af2bd5a74a33844466a1246e3b6
= <gabriel.cirilo.rosa@gmail.com>
2024-08-15 21:35:28 -0300

    [createTask] Implementa Request e Response

---
+++ src/main/java/com/cirion/taskManager/task/application/api/TaskRequest.java
@@ -0,0 +1,16 @@
+package com.cirion.taskManager.task.application.api;
+
+import com.cirion.taskManager.task.domain.PriorityTask;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Value;
+
+@Value
+public class TaskRequest {
+    @NotBlank
+    @Size(min = 4)
+    private String taskName;
+    @NotEmpty
+    private PriorityTask priorityTask;
+}
---
+++ src/main/java/com/cirion/taskManager/task/application/api/TaskResponse.java
@@ -0,0 +1,10 @@
+package com.cirion.taskManager.task.application.api;
+
+import lombok.Value;
+
+import java.util.UUID;
+
+@Value
+public class TaskResponse {
+    private UUID idTask;
+}
